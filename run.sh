ff9351effee2a51e88f7698285ad25cc705a0c6e
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-08 01:53:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java --enable-preview -XX:+ShowCodeDetailsInExceptionMessages -cp "bin;lib/commons-codec-1.15.jar;lib/juniversalchardet-1.0.3.jar;lib/json-20220320.jar" app.SShell
+java --enable-preview -jar jar/SShell.jar

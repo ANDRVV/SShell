fd595460a70089e708e9a4c31ac503793fb1f0c3
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-09 00:06:47 +0200

    Update run.py

--- run.py
+++ run.py
@@ -11,16 +11,20 @@ if "0" in code:
     os.system("pip install requests")
     javaVersion = re.search(r'"[0-9\._]*"', subprocess.check_output("java -version", shell = True, stderr = subprocess.STDOUT).decode().split("\r")[0]).group().replace('"', '')
     if "19" in javaVersion:
-        wver = open("setupVerification", "w")
-        wver.write("1")
-        wver.close()
-        print("[OK] SETUP COMPLETED")
-        response = input("Run SShell? [N/y] ")
-        if response.lower() == "y":
-            if "windows" in platform.platform().lower():
-                os.system("@echo off & cls & java --enable-preview -jar jar\SShell.jar")
-            else:
-                os.system("clear && java --enable-preview -jar jar/SShell.jar &> /dev/null")
+        pcapver = input("Manually install winpcap/npcap. [N/ok] ")
+        if "ok" in pcapver.lower():
+            wver = open("setupVerification", "w")
+            wver.write("1")
+            wver.close()
+            print("[OK] SETUP COMPLETED")
+            response = input("Run SShell? [N/y] ")
+            if response.lower() == "y":
+                if "windows" in platform.platform().lower():
+                    os.system("@echo off & cls & java --enable-preview -jar jar\SShell.jar")
+                else:
+                    os.system("clear && java --enable-preview -jar jar/SShell.jar &> /dev/null")
+        else:
+            pass
     else:
         print("[ERROR] Java SE 19 required.")
 else:

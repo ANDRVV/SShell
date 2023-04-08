e9d2acc1caa79490a1ecab73e6d55ffaf597a819
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-08 15:24:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 import os, re, subprocess, platform
-os.system("pip install pynetsys --user")
-javaVersion = re.search(r'"[0-9\._]*"', subprocess.check_output("java -version", shell = True, stderr = subprocess.STDOUT).decode().split("\r")[0]).group().replace('"', '')
-if "19" in javaVersion:
-    print("[OK] SETUP COMPLETED")
-    response = input("Run SShell? [N/y] ")
-    if response.lower() == "y":
-        if "windows" in platform.platform().lower():
-            os.system("run.bat")
-        else:
-            os.system("bash run.sh")
+sver = open("setupVerification", "r")
+code = sver.read()
+sver.close()
+if code == "0":
+    os.system("pip install pynetsys --user")
+    javaVersion = re.search(r'"[0-9\._]*"', subprocess.check_output("java -version", shell = True, stderr = subprocess.STDOUT).decode().split("\r")[0]).group().replace('"', '')
+    if "19" in javaVersion:
+        wver = open("setupVerification", "w")
+        wver.write("1")
+        wver.close()
+        print("[OK] SETUP COMPLETED")
+        response = input("Run SShell? [N/y] ")
+        if response.lower() == "y":
+            if "windows" in platform.platform().lower():
+                os.system("@echo off & cls & java --enable-preview -jar jar\SShell.jar")
+            else:
+                os.system("clear && java --enable-preview -jar jar/SShell.jar &> /dev/null")
+    else:
+        print("[ERROR] Java SE 19 required.")
 else:
-    print("[ERROR] Java SE 19 required.")
+    if "windows" in platform.platform().lower():
+        os.system("@echo off & cls & java --enable-preview -jar jar\SShell.jar")
+    else:
+        os.system("clear && java --enable-preview -jar jar/SShell.jar &> /dev/null")

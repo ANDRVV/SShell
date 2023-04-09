6ca50ebebd4c65543662d2d708948135a9e52a5c
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-09 17:48:34 +0200

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ if "0" in code:
     os.system("pip install paramiko")
     os.system("pip install requests")
     javaVersion = re.search(r'"[0-9\._]*"', subprocess.check_output("java -version", shell = True, stderr = subprocess.STDOUT).decode().split("\r")[0]).group().replace('"', '')
-    if "19" in javaVersion:
+    if javaVersion is not None:
         pcapver = input("Manually install winpcap/npcap. [N/ok] ")
         if "ok" in pcapver.lower():
             if "windows" in platform.platform().lower():
@@ -30,7 +30,7 @@ if "0" in code:
         else:
             pass
     else:
-        print("[ERROR] Java SE 19 required.")
+        print("[ERROR] Java required.")
 else:
     if "windows" in platform.platform().lower():
         os.system('@echo off & cls & java --enable-preview -cp "bin;lib\\commons-codec-1.15.jar;lib\\juniversalchardet-1.0.3.jar" app.SShell')

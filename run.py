f0159acd7294ce593ead6e4f9eb68a5f8b2191f4
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-24 16:46:28 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ os.chdir(os.path.dirname(__file__))
 sver = open("setupVerification", "r")
 code = sver.read()
 sver.close()
-if not sys.argv[1]:
-    sys.argv[1] = "none"
-if "0" in code and sys.argv[1] == "--bypass":
+if len(sys.argv[1]) < 2:
+    args = "none"
+else: 
+    args = sys.argv[1]
+if "0" in code and args == "--bypass":
     if "windows" in platform.platform().lower():
         os.system("@echo off & javac -d bin --class-path bin --add-modules juniversalchardet,org.apache.commons.codec --module-path lib src\\app\\SShell.java")
     else:
@@ -15,7 +17,7 @@ if "0" in code and sys.argv[1] == "--bypass":
         os.system('@echo off & cls & java --enable-preview -cp "bin;lib\\commons-codec-1.15.jar;lib\\juniversalchardet-1.0.3.jar" app.SShell')
     else:
         os.system('clear && java --enable-preview -cp "bin:lib/commons-codec-1.15.jar:lib/juniversalchardet-1.0.3.jar" app.SShell')
-if "0" in code and sys.argv[1] != "--bypass":
+if "0" in code and args != "--bypass":
     print("Setup in progress...")
     if "windows" in platform.platform().lower():
         os.system("@pip install pynetsys --user")
